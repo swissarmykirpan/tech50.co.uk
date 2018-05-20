5b9e387bb9ede1768a860483c512a0fdffa6c425
swissarmykirpan <gurpreet@gurpreetsohal.com>
2018-05-20 16:46:28 +0100

    remove random text

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -10,7 +10,7 @@ export class Hello extends React.Component<HelloProps, {}> {
     var x = new Test();
     return (
       <h1>
-        Hello from {x.name} and {this.props.compiler} and {this.props.framework}
+        Hello from {x.name}
       </h1>
     );
   }
