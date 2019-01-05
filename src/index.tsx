3ac0635694de446565442a4905d8cdf79d60088f
swissarmykirpan <gurpreet@gurpreetsohal.com>
2019-01-05 01:56:04 +0000

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Hello } from "./components/Hello";
 
-ReactDOM.render(
-  <Hello compiler="Typescript1234Gurpreet" framework="react" />,
-  document.getElementById("root")
-);
+ReactDOM.render(<Hello framework="react" />, document.getElementById("root"));
